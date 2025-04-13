884cc9db209e739cff9a293ece5432d0f63adcd4
qianhui wang <qw304@cam.ac.uk>
2025-04-13 20:45:03 +0100

    check with xml jenkins

--- build-hybrid/test_syncio.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-try:
-    s = socket.create_connection(('127.0.0.1', 16068), timeout=5)
-    print("Synchronous connection succeeded!")
-    s.close()
-except Exception as e:
-    print("Synchronous connection failed:", e)
-
